59b23d8b4b8886feb1a07b2f197f075c04a1ea26
AndriiSolodaiev <andik05016@gmail.com>
2023-09-28 19:29:57 +0300

    create swiper spa-swimming-pool

--- src/assets/scripts/gulp-modules/spa-swimming-pool-wn.js
+++ src/assets/scripts/gulp-modules/spa-swimming-pool-wn.js
@@ -1,7 +1,10 @@
+import gsap from 'gsap/all';
 import Swiper from 'swiper';
+import { Navigation } from 'swiper';
 import 'swiper/css';
 
 const swiper = new Swiper('.swiper', {
+  modules: [Navigation],
   speed: 1000,
   spaceBetween: 0,
   // Navigation arrows
@@ -10,4 +13,22 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
     nextEl: '.swiper-button-next',
   },
+});
+
+const currentSlideShow = [document.querySelector('[data-first-digit]')];
+
+currentSlideShow[0].textContent = swiper.realIndex + 1;
+document.querySelector('[data-total]').textContent = document.querySelectorAll(
+  '.swiper .swiper-slide:not(.swiper-slide-duplicate)',
+).length;
+swiper.on('activeIndexChange', self => {
+  const splitedIndex = self.realIndex + 1;
+  const firstDigit = splitedIndex;
+  gsap
+    .timeline()
+    .fromTo(currentSlideShow[0], { yPercent: 0 }, { yPercent: 100 })
+    .add(() => {
+      currentSlideShow[0].textContent = firstDigit;
+    })
+    .fromTo(currentSlideShow[0], { yPercent: -100 }, { yPercent: 0 });
 });
