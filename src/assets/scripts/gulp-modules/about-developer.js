6454151caa3a775b5ca1f99ac868e3ba58c682e3
AndriiSolodaiev <andik05016@gmail.com>
2023-10-17 19:46:32 +0300

    add bottom tree about-developer

--- src/assets/scripts/gulp-modules/about-developer.js
+++ src/assets/scripts/gulp-modules/about-developer.js
@@ -5,3 +5,20 @@ import '../modules/helpers/imgParallax';
 gsap.registerPlugin(ScrollTrigger, CustomEase);
 
 new App();
+
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: '.projects',
+      start: 'top center',
+    },
+  })
+  .from('.projects__bottom-tree-wrap', { yPercent: -100, autoAlpha: 0, duration: 0.4 })
+  .from('.svg-title__leave--project', {
+    scale: 0,
+    y: 40,
+    x: 50,
+    stagger: 0.1,
+    delay: 0.2,
+    duration: 0.4,
+  });
